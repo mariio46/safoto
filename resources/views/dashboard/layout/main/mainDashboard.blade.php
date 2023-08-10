d4284e817112c2db50ee7b96c9e947d5444ff1bf
Mario <mariomad2296@gmail.com>
2023-08-11 03:22:42 +0800

    second

--- resources/views/dashboard/layout/main/mainDashboard.blade.php
+++ resources/views/dashboard/layout/main/mainDashboard.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>{{ 'Safoto | ' . $title }}</title>
-    <link rel="icon" href="{{ asset('../storage/img/default/title-logo.png') }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset('../storage/image/default/title-logo.png') }}" type="image/x-icon">
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
